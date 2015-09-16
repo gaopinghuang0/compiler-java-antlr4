9042bfdef8d9c29609d7b2ddfeb2e394a8347b08
gaopinghuang0 <gaopinghuang@gmail.com>
2015-09-15 20:03:25 -0400

    update batchTest.sh by using diff -b -B

--- batchTest.sh
+++ batchTest.sh
@@ -4,17 +4,8 @@
 for a in `seq 1 22` ; do
 	infile="test$a.micro"
 	echo $infile
-#	find -name $infile -exec cat {} \;
-	input=$(find -name $infile -exec java -cp lib/antlr.jar:classes/ Micro {} \;)
-	echo $input
-	output=$(find -name "test$a.out" -exec cat {} \;)
-	echo $output
-#	if [ $input = $output ]; then
-#		echo "Pass"
-#	else
-#		echo "False"
-#	fi
+	find -name $infile -exec java -cp lib/antlr.jar:classes/ Micro {} > test_result \;
+	find -name "test$a.out" -exec diff -b -B test_result {} \;
 	echo
 done
-
-#find . -name 'test1.out' -exec cat {} \;
+rm test_result
