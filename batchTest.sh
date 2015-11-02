7203e643496f56476c660a5bc2e387e7bc4fefe7
gaopinghuang0 <gaopinghuang@gmail.com>
2015-11-02 10:56:33 -0500

    update batchTest.sh

--- batchTest.sh
+++ batchTest.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 # bash for loop
-for a in `seq 1 22` ; do
-	infile="test$a.micro"
-	echo $infile
-	find -name $infile -exec java -cp lib/antlr.jar:classes/ Micro {} > test_result \;
-	find -name "test$a.out" -exec diff -b -B test_result {} \;
-	echo
+#for a in `seq 1 22` ; do
+#	infile="test$a.micro"
+#	echo $infile
+#	find -name $infile -exec java -cp lib/antlr.jar:classes/ Micro {} > test_result \;
+#	find -name "test$a.out" -exec diff -b -B test_result {} \;
+#	echo
+#done
+#rm test_result
+
+# bash for loop
+for entry in testcases/input/*.micro
+do
+	set -- "$entry"
+	IFS="."; read -ra ADDR <<< "$entry"
+	echo "${ADDR[0]}.micro"
+	java -cp lib/antlr.jar';'classes/ Micro "${ADDR[0]}.micro" > tempout
+	IFS="/"; read -ra ARRAY <<< "${ADDR[0]}"
+	find -name "${ARRAY[2]}.out" -exec diff -b -B tempout {} \;
+#	echo "${ADDR[0]}.input"
 done
-rm test_result
+rm tempout
