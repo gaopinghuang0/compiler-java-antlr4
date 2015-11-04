36caa6bf51abc3cb219050096e0f7b42df77ec36
gaopinghuang0 <gaopinghuang@gmail.com>
2015-11-03 20:20:45 -0500

    finish step 5

--- batchTest.sh
+++ batchTest.sh
@@ -11,12 +11,15 @@
 #rm test_result
 
 # bash for loop
+# http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+make
 for entry in testcases/input/*.micro
 do
-	set -- "$entry"
+	# split filename by ',' into ${ADDR[@]}, including 2 parts ${ADDR[0]} and ${ADDR[1]}
 	IFS="."; read -ra ADDR <<< "$entry"
 	echo "${ADDR[0]}.micro"
 	java -cp lib/antlr.jar';'classes/ Micro "${ADDR[0]}.micro" > tempout
+	# split path by '/' into ${ARRAY[@]}, including 3 parts
 	IFS="/"; read -ra ARRAY <<< "${ADDR[0]}"
 	find -name "${ARRAY[2]}.out" -exec diff -b -B tempout {} \;
 #	echo "${ADDR[0]}.input"
